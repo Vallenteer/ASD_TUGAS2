b4857cfedd761e06215eecd8cce78eb45dd49282
Vallenteer <kristiantokelvin@yahoo.co.id>
2015-11-19 21:50:37 +0700

    sudah edit distance
    belum disort,
    hasilna lsng keluar console,
    no idea how to sort .-.

--- ASD_TUGAS2/ASD_TUGAS2/Program.cs
+++ ASD_TUGAS2/ASD_TUGAS2/Program.cs
@@ -18,17 +18,86 @@ namespace ASD_TUGAS2
       
         static void Main(string[] args)
         {
-            Console.WriteLine("LOADING....");
-            Baca_hitung();
+            Hashtable kata= Baca_hitung();
+            Console.Clear();
 
             // bkin layar buat masukin kata yang dicari
             // lalu edit distance yang diinginkan
 
+            string select_words;
+            int edit_toleran;
+            Console.Write("Masukan Kata : ");
+            select_words = Console.ReadLine();
+            Console.Write("Masukan Edit Distance ( minimum 0 ) : ");
+            edit_toleran = Convert.ToInt32(Console.ReadLine());
+            
+
+            //buat bandingin dan buat keluarin, not sorting yet...
+            foreach (string key in kata.Keys)
+            {
+                int editdistance = edit_distance(select_words, key);
+                if (editdistance <= edit_toleran)
+                {
+                    Console.WriteLine("{0} \t  {1} \t {2}", key, editdistance, kata[key]);
+                }
+            }
+            
+
             // buat koding untuk edit distance 
             // buat koding untuk sorting
+            Console.ReadLine();
         }
-        public static void Baca_hitung()
+
+        public static int edit_distance( string kata1, string kata2)
         {
+            int asli = kata1.Length;
+            int  banding= kata2.Length;
+            int[,] toleransi = new int[asli + 1, banding + 1];
+
+            // Step 1
+            if (asli == 0)
+            {
+                return banding;
+            }
+
+            if (banding == 0)
+            {
+                return asli;
+            }
+
+            // Step 2
+            for (int i = 0; i <= asli; toleransi[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= banding; toleransi[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= asli; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= banding; j++)
+                {
+                    // Step 5
+                    int cost = (kata2[j - 1] == kata1[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    toleransi[i, j] = Math.Min(
+                        Math.Min(toleransi[i - 1, j] + 1, toleransi[i, j - 1] + 1),
+                        toleransi[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return toleransi[asli, banding];
+
+        
+        }
+        static Hashtable Baca_hitung()
+        {
+
+            Console.WriteLine("LOADING....");
             //pattern buat split nya ketmu buahahah
             string line;
             string pattern = @"[\s\n\p{P}-[']]+";
@@ -44,10 +113,6 @@ namespace ASD_TUGAS2
                 //membuat baris list kata2 //disini yang lama...
                 foreach (string element in result)
                 {
-                    //test biar tau waktu baca, cm butuh paling lama 5 detik,
-                    //Console.WriteLine("a");
-                    // ini yang lama, dibawah ini buat tulis ke file
-                     //Mendata(element);
 
                     //coba pke hashtable
                     if (kata.ContainsKey(element.ToLower()))
@@ -77,7 +142,8 @@ namespace ASD_TUGAS2
             
 
             Console.WriteLine("selesai");
-            Console.ReadLine();
+            return kata;
+            //Console.ReadLine();
             //Console.Clear();
         }
         
