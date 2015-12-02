82b8bf2b865d3b6d8c6e4bf96e506718053dfdc8
Vallenteer <kristiantokelvin@yahoo.co.id>
2015-12-02 22:19:17 +0700

    ud bisa :D
    silakan di debug dan dibkin user manual

--- ASD_TUGAS2/ASD_TUGAS2/Program.cs
+++ ASD_TUGAS2/ASD_TUGAS2/Program.cs
@@ -16,93 +16,104 @@ namespace ASD_TUGAS2
     class Program
     {
 
-        
+
         static void Main(string[] args)
         {
-            //Hashtable kata= Baca_hitung();
-            
-            Console.Clear();
-            // bkin layar buat masukin kata yang dicari
-            // lalu edit distance yang diinginkan
-           
-            string select_words;
+            Hashtable kata = Baca_hitung();
+
+            string select_words, editstring;
             int edit_toleran;
-            Console.Write("Masukan Kata : ");
-            select_words = Console.ReadLine();
-            Console.Write("Masukan Edit Distance ( minimum 0 ) : ");
-            edit_toleran = Convert.ToInt32(Console.ReadLine());
 
-            // array ini bsa di sort, makasih :D tapi lom ada nilai edit distance .-.
-            string[,] kata_array= masuk_kata(select_words,edit_toleran);
+            bool kondisi;
+            //looping user supaya tidak exit tanpa perintahnya user
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Untuk keluar ketikan -1");
+                Console.Write("Masukan Kata : ");
+                select_words = Console.ReadLine();
+                if (select_words == "-1")
+                {
+                    Environment.Exit(0);
+                }
+                do
+                {
+                    Console.Write("Masukan Edit Distance maksimal ( minimum 0 ) : ");
+                    editstring = Console.ReadLine();
+                    kondisi = int.TryParse(editstring, out edit_toleran);
+                    if (kondisi == true)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine("Angka yang anda masukan salah!");
+                    Console.WriteLine("Tekan sembarang untuk memasukan Edit Distance maksimal kembali...");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Untuk keluar ketikan -1");
+                    Console.WriteLine("Masukan Kata : {0}", select_words);
+                } while (kondisi == false);
 
-
-            //for (int i = 0; i < kata_array.GetLength(0); i++)
-            //{
-                
-            //        Console.WriteLine("{0}" +"\t"+"{1}", kata_array[i, 0],kata_array[i,1]);                
-            //}
-
-               
-                //buat bandingin dan buat keluarin, not sorting yet...
-
-
-
-
-                Console.ReadLine();
+                if (edit_toleran == -1)
+                {
+                    Environment.Exit(0);
+                }
+                masuk_kata(select_words, edit_toleran, kata);
+                Console.Write("Tekan Sembarang untuk melihat Edit Distance lagi..");
+                Console.ReadKey();
+            }
         }
 
-        public static string[,] masuk_kata(string select_words, int edit_toleran)
+        public static void masuk_kata(string select_words, int edit_toleran, Hashtable kata)
         {
             List<string> list_kata = new List<string>();
-            List<string> list_distance = new List<string>();
-
-            Hashtable kata = Baca_hitung();
             int counter = 0;
-            //buat bandingin dan buat keluarin, not sorting yet...
-            foreach (string key in kata.Keys)
+            int batas = 0;
+            while (batas <= edit_toleran)
             {
-                int editdistance = edit_distance(select_words, key);
-                if (editdistance <= edit_toleran)
+                foreach (string key in kata.Keys)
                 {
-                    
-                    list_kata.Add((key));
-                    list_distance.Add((editdistance.ToString()));
-                    //Console.WriteLine("{0} \t  {1} \t {2}", key, editdistance, kata[key]);
-                    counter++;
+                    //cari yang sesuai edit distance sesuai batas,
+                    int editdistance = edit_distance(select_words, key);
+                    if (editdistance == batas)
+                    {
+
+                        list_kata.Add((key));
+
+                        counter++;
+                    }
                 }
+                if (counter == 0)
+                {
+                    Console.WriteLine("Tidak ada Kata yang sesuai");
+                    batas = edit_toleran;
+                }
+                else
+                {
+                    //cetak sesuai batas
+                    string[] kata_array = list_kata.ToArray();
+                    Quicksort(kata_array, 0, kata_array.Length - 1);
+                    foreach (string isi in kata_array)
+                    {
+                        Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t", isi, batas, kata[isi]);
+                    }
+                    //menghilangkan isi list untuk berikutnya
+                    list_kata.Clear();
+                }
+                batas++;
             }
-            if (counter == 0)
-            {
-                Console.WriteLine("Tidak ada Kata yang sesuai");
-                
-            }
-            string[,] kata_array = new string [list_kata.Count, list_distance.Count];
-                //=list_kata.ToArray() ;
+            Console.WriteLine("Jumlah Kata yang mempunyai Edit distance maksimal {0} dari kata \"{1}\" adalah = {2}",edit_toleran,select_words,counter );
+
             int panjang1 = list_kata.Count;
-            //Console.WriteLine(list_kata[2]);
 
-
-            for (int i = 0; i < panjang1; i++)
-            {
-                kata_array[i, 0] = list_kata[i];
-                //Console.WriteLine("{0}", kata_array[0, i]);
-                kata_array[i, 1] = list_distance[i];
-            }
-
-            Console.WriteLine("{0}" + "/t" + "{1}", kata_array[0, 0], kata_array[0, 1]);
-            Console.WriteLine("{0}" + "/t" + "{1}", kata_array[1, 0], kata_array[1, 1]);
-            
-
-            return kata_array ;
         }
 
 
-        public static int edit_distance( string kata1, string kata2)
+        public static int edit_distance(string kata1, string kata2)
         {
             // sumber : http://www.dotnetperls.com/
 
             int asli = kata1.Length;
-            int  banding= kata2.Length;
+            int banding = kata2.Length;
             int[,] toleransi = new int[asli + 1, banding + 1];
 
             // Step 1
@@ -143,12 +154,12 @@ namespace ASD_TUGAS2
             // Step 7
             return toleransi[asli, banding];
 
-        
+
         }
         static Hashtable Baca_hitung()
         {
 
-           // Console.WriteLine("LOADING....");
+            Console.WriteLine("LOADING....");
             //pattern buat split nya ketmu buahahah
             string line;
             string pattern = @"[\s\n\p{P}-[']]+";
@@ -161,8 +172,7 @@ namespace ASD_TUGAS2
             while ((line = sr.ReadLine()) != null)
             {
                 string[] result = rgx.Split(line);
-                //membuat baris list kata2 //disini yang lama...
-                //Quicksort(result, 0, result.Length - 1); //harusnya yang baris ini udah bkin sort di sblum hash
+
 
                 foreach (string element in result)
                 {
@@ -178,25 +188,24 @@ namespace ASD_TUGAS2
                     {
                         kata.Add(element.ToLower(), 1);
                     }
-                     
+
                 }
-                
+
 
 
             }
             sr.Close();
-           // Console.WriteLine("selesai");
+            // Console.WriteLine("selesai");
             //Console.Clear();
             return kata;
             //Console.ReadLine();
-            
+
         }
 
         public static void Quicksort(IComparable[] elements, int left, int right)
         {
             int i = left, j = right;
             IComparable pivot = elements[(left + right) / 2];
-
             while (i <= j)
             {
                 while (elements[i].CompareTo(pivot) < 0)
@@ -232,5 +241,7 @@ namespace ASD_TUGAS2
                 Quicksort(elements, i, right);
             }
         }
+
+
     }
 }
